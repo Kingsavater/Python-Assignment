c9032096504e4f8ae5140d495c582ecc7ab8c1e6
daniel <allid5742@gmail.com>
2020-03-19 21:45:53 +0100

    this is the assigment

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,11 @@
 #Add your assignment here
+name=input("Can you drink at :")
+name= ""
+if {name <= "18"} and {name <= "21"}:
+    print("id required")
+elif  {name > "21"} :
+    print("allowed")
+else: {name >= "17"} 
+print("too young")
+ 
+
